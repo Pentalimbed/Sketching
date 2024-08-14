c33006c0c274407a679b3f53d16dd253f0ca46e9
Pentalimbed <pentalimbed@gmail.com>
2024-08-14 13:13:13 +0100

    for colab

--- data.py
+++ data.py
@@ -15,20 +15,20 @@ class ReconDataset(Dataset):
         match src:
             case "imagenet-r":
                 self.ds = (
-                    datasets.load_dataset('axiong/imagenet-r', split='test')
+                    datasets.load_dataset('axiong/imagenet-r', cache_dir=r'datasets\imagenet-r', split='test')
                     .cast_column("image", datasets.Image("RGB"))
                     .select_columns(["image"])
                 )
             case "imagenet":
                 self.ds = (
-                    datasets.load_dataset('ILSVRC/imagenet-1k', split='test',
+                    datasets.load_dataset('ILSVRC/imagenet-1k', cache_dir=r'datasets\imagenet-1k', split='test',
                                           token="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", trust_remote_code=True)
                     .cast_column("image", datasets.Image("RGB"))
                     .select_columns(["image"])
                 )
             case "imagenet":
                 self.ds = (
-                    datasets.load_dataset('PhilSad/celeba-hq-15k', split='test')
+                    datasets.load_dataset('PhilSad/celeba-hq-15k', cache_dir=r'datasets\celeba-hq', split='test')
                     .cast_column("image", datasets.Image("RGB"))
                     .select_columns(["image"])
                 )
